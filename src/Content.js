a28d4b0aac8e2c34d1ee6a0ad715f6d54d0f1b73
FAYE JUSTIN FELICES <felicesfayejustin@gmail.com>
2024-02-19 04:51:55 +0000

    lab activity 3 - conditional rendering

--- src/Content.js
+++ src/Content.js
@@ -1,24 +1,33 @@
-import { Featured } from './Featured';
-import { postData } from './postData';
-import { CompiledPosts } from './CompiledPosts';
+import { Featured } from "./Featured";
+import { CompiledPosts } from "./CompiledPosts";
 
 export function Content() {
-  
-
   return (
-    <div className='content'>
+    <div className="content">
       <div class="search">
-        <form method="GET" action="/archive" class="search__box" autocomplete="off">
-          <label for="search" class="search__label">hello.</label>
-          <input type="text" class="search__bar" name="search"
-            placeholder="looking for something? type it here." value="" fdprocessedid="1t3c1h" />
+        <form
+          method="GET"
+          action="/archive"
+          class="search__box"
+          autocomplete="off"
+        >
+          <label for="search" class="search__label">
+            hello.
+          </label>
+          <input
+            type="text"
+            class="search__bar"
+            name="search"
+            placeholder="looking for something? type it here."
+            value=""
+            fdprocessedid="1t3c1h"
+          />
         </form>
       </div>
       <Featured />
-      
-      <CompiledPosts />
-      <a id="button" class="show" href=''></a>
-    </div>
 
+      <CompiledPosts />
+      <a id="button" class="show" href=""></a>
+    </div>
   );
 }
--- src/Featured.js
+++ src/Featured.js
@@ -1,17 +1,14 @@
-import { Card } from "./Card";
 import { FeaturedCards } from "./FeaturedCards";
 import { featuredPost } from "./featuredPost";
 
-
 export function Featured(props) {
   return (
-    <div className='featured'>
+    <div className="featured">
       <h1 class="title">Featured Entries</h1>
-      <div className='card-container'>
-
+      <div className="card-container">
         {featuredPost.map((entry) => (
-          <FeaturedCards featObj={entry} key={entry.number}/>
-        ))}        
+          <FeaturedCards featObj={entry} key={entry.number} />
+        ))}
       </div>
     </div>
   );
